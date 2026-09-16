1a8093def4d087625ceb9d7148498ed5cb37d83a
masada224 <179183540+masada224@users.noreply.github.com>
2026-09-16 12:05:18 +0900

    プロジェクトファイルを追加。

---
+++ NFC/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace NFC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.comboBoxDevice = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonRead
+            // 
+            this.buttonRead.Location = new System.Drawing.Point(241, 213);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(296, 23);
+            this.buttonRead.TabIndex = 0;
+            this.buttonRead.Text = "読み取り";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            this.buttonRead.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBoxDevice
+            // 
+            this.comboBoxDevice.FormattingEnabled = true;
+            this.comboBoxDevice.Location = new System.Drawing.Point(332, 78);
+            this.comboBoxDevice.Name = "comboBoxDevice";
+            this.comboBoxDevice.Size = new System.Drawing.Size(381, 23);
+            this.comboBoxDevice.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(251, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "スマートカード読み取り機器名";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(132, 242);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(513, 186);
+            this.dataGridView.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBoxDevice);
+            this.Controls.Add(this.buttonRead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRead;
+        private System.Windows.Forms.ComboBox comboBoxDevice;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
+
---
+++ NFC/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PCSC;
+using PCSC.Iso7816;
+using PCSC.Exceptions;
+
+
+namespace NFC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var readerName = comboBoxDevice.Text;  // 選択中のカードリーダ名を得る
+
+            using (var context = ContextFactory.Instance.Establish(SCardScope.System))
+            {
+                try
+                {
+                    using (var rfidReader = context.ConnectReader(readerName, SCardShareMode.Shared, SCardProtocol.Any))
+                    {
+                        // APDUコマンドの作成
+                        var apdu = new CommandApdu(IsoCase.Case2Short, rfidReader.Protocol)
+                        {
+                            CLA = 0xFF,
+                            Instruction = InstructionCode.GetData,
+                            P1 = 0x00,
+                            P2 = 0x00,
+                            Le = 0 // We don't know the ID tag size
+                        };
+                        // 読み取りコマンド送信
+                        using (rfidReader.Transaction(SCardReaderDisposition.Leave))
+                        {
+                            var sendPci = SCardPCI.GetPci(rfidReader.Protocol);
+                            var receivePci = new SCardPCI(); // IO returned protocol control information.
+
+                            var receiveBuffer = new byte[256];
+                            var command = apdu.ToArray();
+
+                            var bytesReceived = rfidReader.Transmit(
+                                sendPci, // Protocol Control Information (T0, T1 or Raw)
+                                command, // command APDU
+                                command.Length,
+                                receivePci, // returning Protocol Control Information
+                                receiveBuffer,
+                                receiveBuffer.Length); // data buffer
+
+                            var responseApdu = new ResponseApdu(receiveBuffer, bytesReceived, IsoCase.Case2Short, rfidReader.Protocol);
+                            if (responseApdu.HasData)
+                            {
+                                // バイナリ文字列の整形
+                                StringBuilder id = new StringBuilder(BitConverter.ToString(responseApdu.GetData()));
+                                id.Replace("-", string.Empty);
+                                // データグリッドビューに結果を追加し、連番をカウントアップする
+                                int seq = dataGridView.Rows.Count + 1;
+                                DateTime dt = DateTime.Now;
+                                dataGridView.Rows.Add(seq, dt.ToString("yyyy/MM/dd HH:mm:ss"), id.ToString());
+                            }
+                            else
+                            {
+                                MessageBox.Show("このカードではIDを取得できません", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            }
+                        }
+                    }
+                }
+                catch (RemovedCardException)
+                {
+                    MessageBox.Show("スマートカードが取り外されたため、通信できません", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                }
+                catch (PCSCException ex)
+                {
+                    MessageBox.Show(ex.Message, "スマートカードエラー", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // リーダーの機器情報を取得
+            try
+            {
+                using (var context = ContextFactory.Instance.Establish(SCardScope.System))
+                {
+                    var readerNames = context.GetReaders();
+                    if (readerNames == null || readerNames.Length == 0)
+                    {
+                        MessageBox.Show("スマートカードリーダが見つかりません", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                        Application.Exit();  // スマートカードリーダが無ければ終了する
+                    }
+                    foreach (var readerName in readerNames)
+                    {
+                        comboBoxDevice.Items.Add(readerName);  // カードリーダの名前をコンボボックスに追加
+                    }
+                }
+            }
+            catch (NoServiceException)
+            {
+                MessageBox.Show("スマートカードリソースマネージャが稼働していません", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                Application.Exit();
+            }
+            comboBoxDevice.SelectedIndex = 0;
+
+            // データグリッドビューの初期設定
+            dataGridView.AllowUserToResizeColumns = true;
+            dataGridView.AllowUserToResizeRows = false;
+            dataGridView.RowHeadersVisible = false;
+            dataGridView.AllowUserToAddRows = false;
+            dataGridView.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            dataGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            dataGridView.Columns.Add("seq", "連番");
+            dataGridView.Columns.Add("datetime", "読み取り日時");
+            dataGridView.Columns.Add("uid", "UID/IDm");
+
+            dataGridView.Columns["seq"].Width = 80;
+            dataGridView.Columns["datetime"].Width = 200;
+            dataGridView.Columns["uid"].Width = 250;
+        }
+    }
+}
